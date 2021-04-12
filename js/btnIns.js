45b8bfedf3dd6ec242632d618591b43c2d10d635
biiidesu <63779640+biiidesu@users.noreply.github.com>
2021-04-13 05:22:36 +0700

    Update btnIns.js

--- js/btnIns.js
+++ js/btnIns.js
@@ -1,3 +1,7 @@
 document.getElementById('btn-ins-index-end').onclick = () => {
     location.href = "https://www.instagram.com/brbrnguyen/";
 };
+
+document.getElementById('btn-blog').onclick = () => {
+    location.href = "blog.html";
+};
